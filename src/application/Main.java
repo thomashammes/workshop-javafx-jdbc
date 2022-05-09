5206f6dd00431bb3a729b5f9e1b69e0e896af59e
Thomas Hammes <thomas.hammes@hotmail.com>
2022-05-08 21:54:14 -0300

    Local project created

---
+++ src/application/Main.java
@@ -0,0 +1,36 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			ScrollPane scrollPane = loader.load();
+			
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
+			
+			Scene mainScene = new Scene(scrollPane);
+			primaryStage.setScene(mainScene);
+			primaryStage.setTitle("POOkéShop");
+			primaryStage.getIcons().add(new Image("C:\\Program Files\\eclipse\\workspace\\javafx-workshop\\src\\img\\icons8-pokemon-go-64.png"));
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module javafx1 {
+	requires javafx.controls;
+	requires javafx.fxml;
+	exports gui;
+	
+	opens application to javafx.graphics, javafx.fxml;
+	opens gui;
+}
